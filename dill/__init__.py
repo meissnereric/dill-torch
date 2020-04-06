0bc30a2cbde2055f6c59312f46c41544c41ae2e9
Eric Meissner <meissner.eric.7@gmail.com>
2020-04-06 15:20:01 +0100

    Add submodules to __init__.py

--- dill/__init__.py
+++ dill/__init__.py
@@ -1,7 +1,8 @@
 """The main module for Dill.
 """
 
-__all__ = ['anet']
+__all__ = ['anet', 'rbf', 'simple_data', 'test_functions', 'training', 'utils', 'viz']
+
 from .rbf import *
 from .simple_data import *
 from .simple_model import *
