3410c699aa97ce33dbb76b908e6ea3fbc2c1f49a
Eric Meissner <meissner.eric.7@gmail.com>
2020-04-08 13:05:03 +0100

    Add verbose to plot_outputs

--- dill/viz.py
+++ dill/viz.py
@@ -3,19 +3,21 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_outputs(outputs, final_layer=False):
+def plot_outputs(outputs, final_layer=False, verbose=False):
     """
     Accepts a torch.Tensor and plots it.
     """
-    print('***************************************')
-    print(outputs)
+    if verbose:
+        print('***************************************')
+        print(outputs)
     if not final_layer:
         for sample in outputs.T:
             sample = sample.detach().numpy()
             plt.plot(sample)
     else:
         plt.plot(outputs.detach().numpy())
-    print('***************************************')
+    if verbose:
+        print('***************************************')
 
 def make_predictions(net, dataloaders):
     predictions = []
