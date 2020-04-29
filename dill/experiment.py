Add ReLU option. Fix weight variance non-use bug.

dill/experiment.py
@@ -1,5 +1,5 @@
 from .simple_data import SinDataset
-from .simple_model import create_model, init_normal_model
+from .simple_model import create_rbf_model, init_normal_rbf_model, create_relu_model, init_relu_model
 from .training import Trainer
 from .utils import get_parameters
 from .viz import visualize_predictions, make_predictions
@@ -15,9 +15,11 @@ gdrive_base_path = '/content/'
 
 def run_experiment(train_samples=30, test_samples=300, learning_rate=1e-4,
                    lr_str="1e4", weight_decay=0, net_width=15, sigma=0.2,
-                   rbfs=2, rbf_init='normal',
+                   hidden_layers=2, init_type='normal',
                    num_epochs=1000, plot=True, gdrive=True, weight_variance=0.01,
-                   seed=42, folder_name='exp_data/', record_rate=10_000):
+                   seed=42, folder_name='exp_data/',
+                   record_rate=1_000, print_rate=10_000,
+                   layer_type='rbf'):
     """
     Experimental code to test for double dip phenomenon.
     Batch size is always the full dataset so SGD == GD.
@@ -33,23 +35,29 @@ def run_experiment(train_samples=30, test_samples=300, learning_rate=1e-4,
         from google.colab import files
 
 
+
     train_dataset = SinDataset(train_samples)
     test_dataset = SinDataset(test_samples, variance=0.0)
     train_loader = DataLoader(train_dataset, batch_size=train_samples)
     test_loader = DataLoader(test_dataset, batch_size=test_samples)
     dataloaders = {'train': train_loader, 'val': test_loader}
 
-    net = create_model(net_width, sigma=sigma, rbfs=rbfs)
-    init_normal_model(net, rbfs=rbfs, rbf_init=rbf_init)
+    if layer_type=='rbf':
+        net = create_rbf_model(net_width, sigma=sigma, hidden_layers=hidden_layers)
+        init_normal_rbf_model(net, hidden_layers=hidden_layers, init_type=init_type, weight_variance=weight_variance)
+    else:
+        net = create_relu_model(net_width, hidden_layers=hidden_layers)
+        init_relu_model(net, weight_variance=weight_variance)
 
     original_basis_params = get_parameters(net, zero_grad=True, param_name='basis')
-    original_rbf0_params = get_parameters(net, zero_grad=True, param_name='rbf0')
+    original_0_params = get_parameters(net, zero_grad=True, param_name='{}0'.format(layer_type))
     original_weight_params = get_parameters(net, zero_grad=False, param_name='weights')
 
     optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate, weight_decay=weight_decay) # full batch size == GD
     criterion = nn.MSELoss()
 
-    trainer = Trainer(dataloaders=dataloaders, model=net, criterion=criterion, optimizer=optimizer)
+    trainer = Trainer(dataloaders=dataloaders, model=net, criterion=criterion, optimizer=optimizer,
+                      record_rate=record_rate, print_rate=print_rate)
 
     trainer.train(num_epochs, verbose=False)
 

dill/rbf.py
@@ -51,7 +51,7 @@ class RBF(nn.Module):
         distances = b
         return self.basis_func(distances)
 
-# RBFs
+# hidden_layers
 
 def gaussian(alpha):
     phi = torch.exp((-1/2)*alpha.pow(2))

dill/simple_model.py
@@ -6,30 +6,48 @@ from .viz import plot_outputs
 import numpy as np
 
 
-def create_model(net_width, sigma=0.2, rbfs=1):
+def create_relu_model(net_width, hidden_layers=1, relu_type='relu'):
+    if relu_type=='relu':
+        relu = nn.ReLU
+    elif relu_type=='softplus':
+        relu = nn.Softplus
+
     net = nn.Sequential()
     net.add_module('basis', nn.Linear(1, net_width, bias=False))
-    for i in range(rbfs):
+    for i in range(hidden_layers):
+        net.add_module('relu{}'.format(i), relu())
+    net.add_module('weights', nn.Linear(net_width, 1, bias=False))
+    return net
+
+def create_rbf_model(net_width, sigma=0.2, hidden_layers=1):
+    net = nn.Sequential()
+    net.add_module('basis', nn.Linear(1, net_width, bias=False))
+    for i in range(hidden_layers):
         net.add_module('rbf{}'.format(i), RBF(net_width, net_width, gaussian, sigma=sigma))
     net.add_module('weights', nn.Linear(net_width, 1, bias=False))
     return net
 
-def init_const_model(model, centres_range=None, rbfs=1):
+def init_const_rbf_model(model, centres_range=None, hidden_layers=1):
     centres_range = (0,2*np.pi) if centres_range is None else centres_range
     apply_init(model, 'basis', constant_init(constant=1))
     apply_init(model, 'rbf0', range_init(start=centres_range[0], end=centres_range[1]))
-    for i in range(rbfs - 1):
+    for i in range(hidden_layers - 1):
         apply_init(model, 'rbf{}'.format(i+1), constant_init(constant=1))
     apply_init(model, 'weights', constant_init(constant=1))
 
-def init_normal_model(model, weight_variance=0.01, centres_range=None, rbfs=1, rbf_init='normal'):
+
+def init_relu_model(model, weight_variance=0.01):
+    apply_init(model, 'basis', constant_init(constant=1))
+    apply_init(model, 'weights', normal_init(variance=weight_variance))
+
+def init_normal_rbf_model(model, weight_variance=0.01, centres_range=None, hidden_layers=1, init_type='normal'):
     centres_range = (0,2*np.pi) if centres_range is None else centres_range
     apply_init(model, 'basis', constant_init(constant=1))
     apply_init(model, 'rbf0', range_init(start=centres_range[0], end=centres_range[1]))
-    for i in range(rbfs - 1):
-        if rbf_init == 'normal':
+    for i in range(hidden_layers - 1):
+        if init_type == 'normal':
             apply_init(model, 'rbf{}'.format(i+1), normal_init(variance=weight_variance))
-        elif rbf_init == 'range':
+        elif init_type == 'range':
             apply_init(model, 'rbf{}'.format(i+1), range_init(start=centres_range[0], end=centres_range[1]))
         else:
             apply_init(model, 'rbf{}'.format(i+1), constant_init(constant=1))

dill/test_functions.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 from .viz import plot_outputs
-from .simple_model import create_model, init_const_model
+from .simple_model import create_rbf_model, init_const_rbf_model
 import torch.nn.init as init
 from .rbf import RBF, gaussian
 from .utils import constant_init, apply_init
@@ -27,7 +27,7 @@ def test_layer_two_outputs(data, net_width=5, sigma=0.2):
 
 # What is the final output at layer 3
 def test_full_simple_model(data, net_width=5, sigma=0.2):
-    net = create_model(net_width, sigma)
-    init_const_model(net)
+    net = create_rbf_model(net_width, sigma)
+    init_const_rbf_model(net)
     outputs = net(data)
     plot_outputs(outputs, final_layer=True)

tst/test_experiment.py
@@ -5,14 +5,14 @@ data_folder='tst/test_outputs'
 trainer1, preds1, train1, test1 = run_experiment(train_samples=30, test_samples=300, learning_rate=1e-4,
                    lr_str="1e4", weight_decay=0, net_width=15, sigma=0.2,
                    num_epochs=100, plot=False, gdrive=False, weight_variance=0.01,
-                   rbfs=1,
+                   hidden_layers=1,
                    folder_name=data_folder+'/')
 
 
 trainer2, preds2, train2, test2 = run_experiment(train_samples=30, test_samples=300, learning_rate=1e-4,
                   lr_str="1e4", weight_decay=0, net_width=10, sigma=0.2,
                   num_epochs=10, plot=False, gdrive=False, weight_variance=0.1,
-                  rbfs=1,
+                  hidden_layers=1,
                   folder_name=data_folder+'/')
 
 print("Train datasets equal: ", np.all(np.equal(train1, train2)))